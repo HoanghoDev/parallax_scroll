a4be94eeb7a9eb3e6fc5cd6d712122e7cd07317e
hohoang <hohoang.dev@gmail.com>
2023-05-05 10:57:31 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+let listBg = document.querySelectorAll('.bg');
+let listTab = document.querySelectorAll('.tab');
+let titleBanner = document.querySelector('.banner h1');
+window.addEventListener("scroll", (event) => {
+    /*scrollY is the web scrollbar position (pixel)*/
+    let top = this.scrollY;
+    /*index is the order of classes bg (0,1,2,3,4,5,6,7,8)
+    When scrolling the web, the classes bg scroll down,
+    the bigger the index, the faster the movement
+    */
+    listBg.forEach((bg, index) => {
+        if(index != 0 && index != 8){
+            bg.style.transform = `translateY(${(top*index/2)}px)`;
+        }else if(index == 0){
+            bg.style.transform = `translateY(${(top/3)}px)`;
+        }
+    })
+    titleBanner.style.transform = `translateY(${(top*4/2)}px)`;
+
+    /* parallax scroll in tab
+    when tab's position is less than 550 px
+    from scrollbar position add active class to animate 
+    and vice versa
+    */
+    listTab.forEach(tab =>{
+        if(tab.offsetTop - top < 550){
+            tab.classList.add('active');
+        }else{
+            tab.classList.remove('active');
+        }
+    })
+});  
